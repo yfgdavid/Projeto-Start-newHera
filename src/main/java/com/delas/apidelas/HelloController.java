bce01ad11115b09708725c2053372e5eba3e3c1b
David Victor <davidvictorcontato7@gmail.com>
2024-11-09 20:22:38 -0300

    Update HelloController.java

--- src/main/java/com/delas/apidelas/HelloController.java
+++ src/main/java/com/delas/apidelas/HelloController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // vai lidar com requisicoes HTTP.
-@RequestMapping("/api") // define que todos os endpoints dessa classe vao começar com /api
+@RestController 
+@RequestMapping("/api") 
 public class HelloController {
 
-    @GetMapping("/hello") // define que o método sayHello será acionado quando a URL /api/hello for acessada via método GET.
+    @GetMapping("/hello") 
     public String sayHello() {
         return "Hello, World!";
     }
